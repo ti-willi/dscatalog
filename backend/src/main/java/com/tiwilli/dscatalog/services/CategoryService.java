b85094969a0783837984ea36199824bc6922fd74
Willian Comiotto <willianbcomiotto@gmail.com>
2024-01-06 18:34:49 -0300

    database seeding

--- backend/src/main/java/com/tiwilli/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/tiwilli/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.tiwilli.dscatalog.entities.Category;
 import com.tiwilli.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
     }
