aae70f836632bf32801fa8d9b9ef47f86d2da00b
Willian Comiotto <willianbcomiotto@gmail.com>
2024-01-13 17:46:05 -0300

    Custom contraintValidator

--- backend/src/main/java/com/tiwilli/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/tiwilli/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.tiwilli.dscatalog.dto;
 
+import com.tiwilli.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
     private String password;
